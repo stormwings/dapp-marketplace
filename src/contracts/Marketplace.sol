b9d7783db588d879d8d887fccdd741dda5c74d1f
Mariano Villarreal <marianov994@gmail.com>
2022-03-03 13:41:00 -0300

    add Product's struct and emit event

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,10 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.16;
 
+import "./Product.sol";
+
+event ProductCreated(
+    uint id,
+    string name,
+    uint price,
+    address owner,
+    bool purchased
+);
+
+struct Product {
+    uint id;
+    string name;
+    uint price;
+    address owner;
+    bool purchased;
+}
+
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+
+    mapping(uint => Product) public products;
 
     constructor() public {
         name = "Dapp University Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        // Require a valid name
+        require(bytes(_name).length > 0);
+        // Require a valid price
+        require(_price > 0);
+        // Increment product count
+        productCount ++;
+        // Create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
