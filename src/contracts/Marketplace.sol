90321fb490f7c57754808abfea6c491c9020793a
Mariano Villarreal <marianov994@gmail.com>
2022-03-03 13:19:53 -0300

    initial values

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract Marketplace {
+    string public name;
+
+    constructor() public {
+        name = "Dapp University Marketplace";
+    }
+}
