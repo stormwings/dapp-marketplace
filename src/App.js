36ae28a904dfdb8d9c30e8ccac4a626f34a7b351
Mariano Villarreal <marianov994@gmail.com>
2022-03-09 14:06:36 -0300

    new SocialNetwork contract, updates and App.js to hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from "react";
-
+import React from 'react';
 import Web3 from 'web3'
 import './App.css';
+import Marketplace from './abis/Marketplace.json'
 
 import Navbar from './components/Navbar'
 import Main from './components/Main'
-
-import Marketplace from './abis/Marketplace.json'
+import MetamaskAlert from './components/MetamaskAlert';
 
 const App = () => {
-	const [account, setAccount] = React.useState('');
+  const [account, setAccount] = React.useState('')
+  const [productCount, setProductCount] = React.useState(0)
+  const [products, setProducts] = React.useState([])
+  const [marketplace, setMarketplace] = React.useState(null)
+  const [loading, setLoading] = React.useState(true)
+  const [metamaskInstalled, setMetamaskInstalled] = React.useState(null)
 
-	const [productCount, setProductCount] = React.useState(0);
+  React.useEffect(() => {
+      const metamaskIsInstalled = typeof window.web3 !== 'undefined'
+      setMetamaskInstalled(metamaskIsInstalled)
+  
+      if (metamaskIsInstalled) loadData()
+  }, [])
 
-	const [products, setProducts] = React.useState([]);
-
-	const [marketplace, setMarketplace] = React.useState(null);
-
-	const initialLoading = async () => {
-		await loadWeb3()
+  const loadData = async () => {
+    await loadWeb3()
     await loadBlockchainData()
-	}
-
-	React.useEffect(() => {
-		initialLoading()
-	}, [])
-
-	// console.log({ productCount })
+  }
 
   const loadWeb3 = async () => {
     if (window.ethereum) {
@@ -41,74 +40,84 @@ const App = () => {
     }
   }
 
-  const loadBlockchainData = async () =>  {
+  const loadBlockchainData = async () => {
     const web3 = window.web3
+
     // Load account
     const accounts = await web3.eth.getAccounts()
     setAccount(accounts[0])
 
     // Load network data
     const networkId = await web3.eth.net.getId()
-		// const chainId = await web3.eth.getChainId();
-
     const networkData = Marketplace.networks[networkId]
-
-    // console.log(networkData)
 
     if (networkData) {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      const productCount = await marketplace.methods.productCount().call()
-      
       setMarketplace(marketplace)
+
+      
+      const productCount = await marketplace.methods.productCount().call()
       setProductCount(productCount)
-
-			let p = []
-
+  
       // Load products
       for (var i = 1; i <= productCount; i++) {
-				const product = await marketplace.methods
-					.products(i)
-					.call()
-
-				p = [...p, product]
+        const product = await marketplace.methods.products(i).call()
+        setProducts([...products, product])
       }
 
-			setProducts(p)
-			
+      setLoading(false)
     } else {
-			window.alert('Marketplace contract not deployed to detected network.')
+      window.alert('Marketplace contract not deployed to detected network.')
     }
   }
 
-	// console.log(products)
-
   const purchaseProduct = (id, price) => {
-    marketplace.methods
-			.purchaseProduct(id)
-			.send({ from: account, value: price })
-    	.once('receipt', (receipt) => {})
+    setLoading(true)
+    marketplace.methods.purchaseProduct(id).send({ from: account, value: price })
+    .once('receipt', (receipt) => {
+      setLoading(false)
+    })
   }
 
   const createProduct = (name, price) => {
+    setLoading(true)
+    
     marketplace.methods
       .createProduct(name, price)
       .send({ from: account })
       .once('receipt', (receipt) => {
+        console.log({ receipt })
+        setLoading(false)
       })
   }
 
+  // tipPost(id, tipAmount) {
+  //   console.log("tipping post", id, tipAmount)
+  //   setLoading(true)
+  //   this.state.socialNetwork.methods
+  //     .tipPost(id)
+  //     .send({ from: this.state.account, value: tipAmount })
+  //     .once('receipt', (receipt) => {
+  //     setLoading(false)
+  //   })
+  // }
+  
   return (
     <div>
       <Navbar account={account} />
-      <main role="main" className="col-lg-12 d-flex">
-        <Main
-          products={products}
-          createProduct={createProduct}
-          purchaseProduct={purchaseProduct}
-        />
-      </main>
+      {
+        !loading && 
+        <main role="main" className="col-lg-12 d-flex">
+            { metamaskInstalled ? <Main
+            products={products}
+            productCount={productCount}
+            createProduct={createProduct}
+            purchaseProduct={purchaseProduct}
+          /> : <MetamaskAlert />}
+        </main>
+      }
     </div>
   );
-};
+}
 
 export default App;
